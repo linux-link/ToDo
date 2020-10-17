9214dbcde8c3d9a688ea37eb5ae502b777014397
linux-link <18911560821@qq.com>
2020-10-17 18:43:12 +0800

    ToDo: add other modules
    1.添加剩余的组件.

---
+++ ct_mine/src/main/java/com/wujia/todo/mine/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wujia.todo.mine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mine_activity_main);
+    }
+}
---
+++ ct_plan/src/main/java/com/wujia/todo/plan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wujia.todo.plan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pl_activity_main);
+    }
+}
