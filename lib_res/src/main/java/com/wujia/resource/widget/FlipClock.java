2304c3f183512964734f89ba13910b0dc16fea22
linux-link <18911560821@qq.com>
2020-10-18 22:15:04 +0800

    ToDo: add FlipColcok base framework
    1.添加可翻页时钟的基础组件.

---
+++ lib_res/src/main/java/com/wujia/resource/widget/FlipClock.java
@@ -0,0 +1,27 @@
+package com.wujia.resource.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 翻页时钟.
+ *
+ * @author WuJia.
+ * @version 1.0
+ * @date 2020/10/17
+ */
+public class FlipClock extends LinearLayout {
+
+    public FlipClock(Context context) {
+        super(context, null);
+    }
+
+    public FlipClock(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
---
+++ lib_res/src/main/java/com/wujia/resource/widget/TabDigit.java
@@ -0,0 +1,30 @@
+package com.wujia.resource.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 翻页显示组件
+ *
+ * @author WuJia.
+ * @version 1.0
+ * @date 2020/10/18
+ */
+public class TabDigit extends View implements Runnable {
+
+    public TabDigit(Context context) {
+        super(context, null);
+    }
+
+    public TabDigit(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
